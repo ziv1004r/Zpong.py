27a66fd88b13b1af962b385a941a89b267e7ac9a
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-01 14:17:50 +0200

    t is the best

--- urs.py
+++ urs.py
@@ -52,7 +52,7 @@ clock = pygame.time.Clock()
 
 
 screen_width = 1280
-screen_height = 960
+screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Z-pong')
 
